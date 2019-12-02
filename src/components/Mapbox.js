c594295c0d3bb1d517e686fd298e642a02ed0e81
robertlovin23 <robert.a.lovin@gmail.com>
2019-12-02 15:39:42 -0600

    icon switch

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import './CardStyle.css'
+import stops from '../api/stops'
 
-const Markers = () => {
-    return(
-        <div>
-            <i className="big bus icon"/>
-        </div>
-    )
+const Markers = ({stopMarker}) => {
+    if(stopMarker.served_by_vehicle_types[0] === "bus"){
+        return(
+            <div>
+                <i className="big bus icon"/>
+            </div>
+        )
+    } else if(stopMarker.served_by_vehicle_types[0] === "rail"){
+        return(
+            <div>
+                <i className="big train icon"/>
+            </div>
+        )
+    } else if(stopMarker.served_by_vehicle_types[0] === "tram" || stopMarker.served_by_vehicle_types[0] === "metro"){
+        return(
+            <div>
+                <i className="big subway icon"/>
+            </div>
+        )
+    } else if(stopMarker.served_by_vehicle_types[0] === "ferry"){
+        return(
+            <div>
+                <i className="big ship icon"/>
+            </div>
+        )
+    } else {
+        return(
+            <div>
+                <i className="big bus icon"/>
+            </div>
+        )
+    }
 }
-const Mapbox = ({stops,stop,lat,lng,defaultZoom}) => {
+class Mapbox extends React.Component{
+    state ={
+        routePattern: []
+    }
+    componentDidMount = async () => {
+        const response = await stops.get('/route_stop_patterns')
+        this.setState({
+            routePattern: response.data
+        })
+        console.log(this.state.routePattern)
+    }
+    render(){
+    const { stops,stop,lat,lng,defaultZoom } = this.props
     console.log(stop)
         if(!stop){
             return(
@@ -37,6 +76,7 @@ const Mapbox = ({stops,stop,lat,lng,defaultZoom}) => {
                             stops.map((stop,index) => {
                                 return(
                                     <Markers key={index} text="Marker"
+                                            stopMarker={stop}
                                             lat={stop.geometry.coordinates[1]}
                                             lng={stop.geometry.coordinates[0]}
         
@@ -50,5 +90,5 @@ const Mapbox = ({stops,stop,lat,lng,defaultZoom}) => {
         }
 
     }
-
+}
 export default Mapbox;
